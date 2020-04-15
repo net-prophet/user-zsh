9f44555b5c078615960fa67276ad3d39b6e2260b
Lee Bailey <l@lwb.co>
2020-04-15 15:47:09 -0700

    fixing gpg agent

--- .zsh/config/ssh-gpg-agent.zsh
+++ .zsh/config/ssh-gpg-agent.zsh
@@ -1,3 +1,7 @@
 export GPG_TTY="$(tty)"
-export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket &>/dev/null)
+unset SSH_AGENT_PID
+if [ "${gnupg_SSH_AUTH_SOCK_by:-0}" -ne $$ ]; then
+  export SSH_AUTH_SOCK="/run/user/$UID/gnupg/S.gpg-agent.ssh"
+  #export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket &>/dev/null)
+fi
 gpgconf --launch gpg-agent &>/dev/null
