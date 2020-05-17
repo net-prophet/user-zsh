92e8f4981be887658740aa9ecefe91f959c8a336
Lee Bailey <l@lwb.co>
2020-05-17 12:14:58 -0700

    adding gopath

--- .zsh/config/path.zsh
+++ .zsh/config/path.zsh
@@ -1,2 +1,5 @@
 # Add home directory bin folder
 export PATH="./bin:./node_modules/.bin:$HOME/.local/bin:$HOME/.config/bin:$PATH"
+export GOPATH="$HOME/p/go"
+
+export PATH="/usr/local/go/bin:$PATH"
