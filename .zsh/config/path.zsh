2764fc87fe3a0868b846a89eb6bb51fd53e8f2dc
Leeward Bound <l@lwb.co>
2020-08-01 09:39:02 -0700

    updating path

--- .zsh/config/path.zsh
+++ .zsh/config/path.zsh
@@ -1,6 +1,7 @@
 # Add home directory bin folder
 export PATH="./bin:./node_modules/.bin:$HOME/.local/bin:$HOME/.config/bin:$PATH"
 
+export GO111MODULE=on
 export GOPATH="$HOME/p/go"
 
 export PATH="/usr/local/go/bin:$GOPATH/bin:$PATH"
